1a3ae5561137ff2d07d8e76e97def7305bdb005a
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-12 23:11:40 +0200

    feta: esercizio 2 , created 2 functio, one fot "pari "dispari2 the oder for "randomicoFinoACinque"

--- function.js
+++ function.js
@@ -1,14 +1,32 @@
 //crea una funzione per analizzare se la parola inserita dall'utente è una parola palindroma.
-function palindroma(parola1, parola2) {
+function palindroma(parola1) {
 // Scomposizione della parola in singoli caratteri
-let caratteri = parola.split('');
+let caratteri = parola1.split('');
 // Creazione di una versione invertita della parola
 let invertita = caratteri.reverse().join('');
 // Verifica se la parola è palindroma
-if (parola === invertita) {
-	return(parola + " è una parola palindroma.");
+if (parola1 === invertita) {
+	return(parola1 + " è una parola palindroma.");
 } else {
-	return(parola + " non è una parola palindroma.");
+	return(parola1 + " non è una parola palindroma.");
+}  
 }
-    
+
+
+//ESERCIZIO 2 //
+
+//creiamo una funzione che mi genera un numero randomico tra 1 e 5
+function pariODispari(number){
+	if(number % 2 === 0){
+		return "pari";
+	}else{
+		return "dispari";
+	}
+}
+
+// creiamo una funzione che mi generi un numero randomico tra 1 e 5
+function randomicoFinoACinque (nr){
+	let numeroGenerico = nr;
+	numeroGenerico =  Math.floor( Math.random() * 5);
+	return numeroGenerico;
 }
--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 //Palidroma. Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma.
 
 //chiediamo all'utente di inserire una parola. Dichiarazione della parola da analizzare
-let parola = prompt('Inserisci una parola');
-console.log(parola);
-//Associo una variabile alla mia funzione che analizza la parola inserita dall'utente 
-let risultato = palindroma(parola);
-// stampiamo il risultato nel mio foglio html
-let contenuto = document.getElementById('parola_palindorma');
-let tagVerifica = `           
-        <div class="word">
-        ${risultato}
-        </div>`;
-contenuto.innerHTML += tagVerifica;
+
+// let parolaUtente = prompt('Inserisci una parola');
+// console.log(parolaUtente);
+// //Associo una variabile alla mia funzione che analizza la parola inserita dall'utente 
+// let risultato = palindroma(parolaUtente);
+// // stampiamo il risultato nel mio foglio html
+// let contenuto = document.getElementById('parola_palindorma');
+// let tagVerifica = `           
+//         <div class="word">
+//         ${risultato}
+//         </div>`;
+// contenuto.innerHTML += tagVerifica;
+
+//ESERCIZIO 2 //
+// chiediamo all'utente di seciere un numero da 1 a 5 
+let choise = prompt('Scegli "pari" o "dispari"');
+let nrUtente = prompt('Scegli un numero tra 1 e 5');
+console.log(choise,nrUtente);
+
+
+const Computer = randomicoFinoACinque;
+console.log(nrComputer);
